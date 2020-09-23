92f54209e47f0d95780f33635b35f51974b8d63a
Le <suportejump@gmail.com>
2020-09-23 17:33:17 -0300

    UPD Projetos/DLP

--- Projetos/DLP.c
+++ Projetos/DLP.c
@@ -4,50 +4,119 @@ O programa deve receber um arquivo e devolver se nesse arquivo tem a palavra dig
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <regex.h> // Para utilizar a função regcomp() e a regexec()
+#include <unistd.h> // Para utilizar a função sleep();
 
-// Função que vai receber um caracter e retornar ele em Maiusculo
-char uppercase(char up) {
+// Macro para colocar uma linha pontilhada para fazer uma "separação melhor" e melhor visualização
+#define PONTO puts("--------------------=====================================--------------------");
+#define SEPARQ puts("-----------================COUNTEUDO NO ARQUIVO=====================------------"); printf("\n");;
 
-    if(up >= 'a' && up <= 'z') {
+void printf_result(int value) {
 
-        up = up - 32;
+    // If pattern found
+    if(value == 0) {
+
+        puts("Pattern found!");
 
     }
 
-    return up;
+    // If pattern not found 
+    else if(value == REG_NOMATCH) {
+
+        puts("Pattern not found!");
+
+    }
+
+    // If error occured during Pattern Match
+    else {
+
+        puts("An error occurred!");
+    }
+
 }
 
 int main(int argc, char *argv[]) {
 
     FILE *fh;
 
-    char frase[100]; // = "Cacareco no marreco";
-    char *palaUser = NULL;
-    
-    char letra, lmaiuscula, fmaiuscula;
+    char texto[500]; 
+    char frase[40];
 
-    int cont = 0, tamanhoFrase;
+    int value; // value irá armazenar o valor do retorno da função regcomp();
+
+    regex_t reg; // Variável para criar a regex
+
+    const char *pattern = "[A-Z]"; // REgex
 
     fh = fopen(argv[1], "r"); // fopen é uma função que abre o arquivo. Parâmetros "r" da função fopen para abrir um arquivo para ler
 
     if (fh == NULL) {
 
-        puts("Arquivo não encontrado ou sem permissão de leitura!");
+        perror("Error in opening the file");
         exit(1);
     }
 
-    if (fgets(frase, 100, fh) != NULL) {
+    fread(texto, sizeof(char), 500, fh);
 
-        puts(frase); // writing content to stdout
+    // if (fgets(frase, 100, fh) != NULL) {
 
-    }
+    //   >  puts(frase); // writing content to stdout
+
+    // }
 
     // if (fread(buffer, 32, 1, fh)) { // Função para ler um arquivo
 
     //     // fatal("Não consegui ler os 32 bytes do arquivo");
 
     // } 
+
+    printf("Qual a String que está procurando: ");
+    fgets(frase, 40, stdin);
+
+    printf("Palavra que será procurada: %s\n", frase);
+
+    SEPARQ
+    printf("------ %s\n", texto);
+    SEPARQ
+
+    value = regcomp(&reg, pattern, 0); 
+
+
+    // If compilation is successful
+    if(value == 0) {
+
+        PONTO
+        puts("Regex Compiled Successfuly!");
+        PONTO
+    }
+
+    // Else for compilation error
+    else {
+
+        PONTO
+        perror("Compilation Error!");
+        PONTO
+
+    }
+
+    // O processo irá dormir por 3 segundos
+    // Maybe you're asking yourself " But, why? " Because it looks cool!
+    sleep(3);
+
+    value = regexec(&reg, texto, 0, NULL, 0); // Função usada para dar match em uma string contra um padrão. regexec(&regex, expression, 0, NULL, 0);
+
+    printf_result(value); // Chamando a função para printar na tela o resultado 
+    PONTO
+
+    //printf("%d\n", value);
+
+    if(value == 0) {
+
+        printf("Nome do arquivo na qual deu Match: %s\n", argv[1]);
+
+    }
+
+    regfree(&reg);
 
     fclose(fh); // Fecha o arquivo que foi aberto
 
